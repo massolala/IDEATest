4accb5d7540cb33273b8064b5496ed45eb6ebc32
massolala <823786251@qq.com>
2020-11-17 15:32:26 +0800

    second update

--- src/PackageTest/ClassTest.java
+++ src/PackageTest/ClassTest.java
@@ -3,5 +3,6 @@ package PackageTest;
 public class ClassTest {
     public static void main(String[] args){
         System.out.print("IDEA Test");
+        System.out.print("second commit");
     }
 }
---
+++ src/PackageTest/GitTest.java
@@ -0,0 +1,7 @@
+package PackageTest;
+
+public class GitTest {
+    public static void main(String[] args){
+        System.out.print("git test");
+    }
+}
